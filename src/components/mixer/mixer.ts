f202de3e8289f6309e1bf6194a49416263fd7ab4
Leszek Pietrzak <smutnyleszek@gmail.com>
2018-02-15 12:21:38 +0100

    use syllabler to mix words

--- src/components/mixer/mixer.ts
+++ src/components/mixer/mixer.ts
@@ -1,37 +1,56 @@
+import { syllabler } from "../syllabler/syllabler";
+import { writing } from "../syllabler/writing";
+
 class Mixer {
-  private static readonly vovels: string[] = ["a", "e", "i", "o", "u", "y"];
-
-  public mixWords(firstWord: string, secondWord: string): string[] {
-    const finalMixes = [];
-
-    for (let stIndex = 0; stIndex < firstWord.length; stIndex++) {
-      const stLetter = firstWord[stIndex];
-
-      for (let ndIndex = 0; ndIndex < secondWord.length; ndIndex++) {
-        const ndLetter = secondWord[ndIndex];
-
-        // we're want to glue togeter parts with the same letter
-        // or if both are vovels
-        if (
-          stLetter === ndLetter ||
-          (this.isVovel(stLetter) && this.isVovel(ndLetter))
-        ) {
-          const beginningPart = firstWord.slice(0, stIndex);
-          const endingPart = secondWord.slice(ndIndex, secondWord.length);
-
-          // avoid two letter mixes and duplicates
-          const merged = beginningPart + endingPart;
-          if (merged.length > 2 && finalMixes.indexOf(merged) === -1) {
-            finalMixes.push(merged);
-          }
-        }
-      }
-    }
+  public mixWords(stWord: string, ndWord: string): string[] {
+    let finalMixes: string[] = [];
+    finalMixes = finalMixes.concat(this.mixBySyllables(stWord, ndWord));
+    finalMixes = finalMixes.concat(this.mixBySimilarLetters(stWord, ndWord));
     return finalMixes;
   }
 
-  private isVovel(letter: string): boolean {
-    return Mixer.vovels.indexOf(letter) !== -1;
+  private mixBySyllables(stWord: string, ndWord: string): string[] {
+    const mixes = [];
+    const stSyllables = syllabler.split(stWord);
+    const ndSyllables = syllabler.split(ndWord);
+
+    for (let stIndex = 0; stIndex < stSyllables.length; stIndex++) {
+      const stSyllable = stSyllables[stIndex];
+      for (let ndIndex = 0; ndIndex < ndSyllables.length; ndIndex++) {
+        const ndSyllable = ndSyllables[ndIndex];
+        mixes.push(
+          [
+            stSyllables.slice(0, stIndex + 1).join(""),
+            ndSyllables.slice(ndIndex).join("")
+          ].join("")
+        );
+      }
+    }
+    return mixes;
+  }
+
+  // we're want to glue togeter parts with the same letter
+  // or if both are vovels
+  private mixBySimilarLetters(stWord: string, ndWord: string): string[] {
+    const mixes = [];
+    for (let stIndex = 0; stIndex < stWord.length; stIndex++) {
+      const stLetter = stWord[stIndex];
+      for (let ndIndex = 0; ndIndex < ndWord.length; ndIndex++) {
+        const ndLetter = ndWord[ndIndex];
+
+        if (
+          stLetter === ndLetter ||
+          (writing.isVowel(stLetter) && writing.isVowel(ndLetter))
+        ) {
+          const beginningPart = stWord.slice(0, stIndex);
+          const endingPart = ndWord.slice(ndIndex, ndWord.length);
+
+          // avoid two letter mixes and duplicates
+          mixes.push(beginningPart + endingPart);
+        }
+      }
+    }
+    return mixes;
   }
 }
 
