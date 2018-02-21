9d8f4466b63d28d853f82a8a706cf2ccb92c6c88
Leszek Pietrzak <smutnyleszek@gmail.com>
2018-02-21 09:47:43 +0100

    log syllables in console and empty strings avoiding

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,11 +1,12 @@
 import { mixer } from "../components/mixer/mixer";
+import { syllabler } from "../components/syllabler/syllabler";
 
 const buildMixes = (words: string[]): string[] => {
   let finalMixes: string[] = [];
   words.forEach((word1: string, index1: number) => {
     words.forEach((word2: string, index2: number) => {
-      // don't mix word with itself
-      if (index1 !== index2) {
+      // don't mix word with itself or empty strings
+      if (index1 !== index2 && word1.length * word2.length !== 0) {
         finalMixes = finalMixes.concat(mixer.mixWords(word1, word2));
       }
     });
@@ -13,9 +14,18 @@ const buildMixes = (words: string[]): string[] => {
   return finalMixes;
 };
 
+const logInfo = (words: string[]): void => {
+  const syllablesLog: any = {};
+  for (const word of words) {
+    syllablesLog[word] = syllabler.split(word).join("•");
+  }
+  console.debug("current syllables", syllablesLog);
+};
+
 export const mutations = {
   setWords(state: IState, payload: string[]): void {
     state.words = payload;
     state.mixes = buildMixes(state.words);
+    logInfo(state.words);
   }
 };
